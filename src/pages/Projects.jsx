01ceeefe06bcaad9ec08707711209cc2eaea8c8b
Radha Malde <rmalde@umich.edu>
2026-07-01 16:35:55 -0400

    project 1

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,7 +3,10 @@ function Projects() {
   return (
     <main>
       <h1>Projects Built By Students</h1>
-      <p>.</p>
+      <h2>Motion Detecting Alarm</h2>
+      <p> Students built an alarm that would play a sound when you waved your hand over it. 
+        They used the concepts of parallel and series circuits, as well as programmed it using Arduino's IDE</p>
+      <p></p>
     </main>
   )
 }
